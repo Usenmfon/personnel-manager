24a6830698d8c32eab06f0e6f25615de7e4a827e
orion <carlasparsh@gmail.com>
2022-01-19 21:09:46 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import { Route, Routes, useRoutes } from 'react-router-dom';
+import {  useRoutes } from 'react-router-dom';
 
 
 import { AuthProvider } from "./context/auth";
 
 import { ThemeProvider } from "@mui/system";
 import { theme } from "./components/theme";
-
-import Homepage from "./components/Homepage";
-import Signupsuccess from "./components/Signupsuccess";
-import Listing from "./components/Listing"
-import Farming from "./category/Farming";
-import Login from "./components/Login"
-import SingleStaff from "./components/SingleStaff";
-import AddStaff from "./components/AddStaff";
-import AllStaff from "./components/AllStaff";
 
 import DefaultRoutes from './router';
 
